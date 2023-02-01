bb61f49ac20ed98eae5f2f8e9221711d874f55f0
xwedea <mecitjiyan@gmail.com>
2023-02-01 14:07:18 -0500

    inventory

--- characters/cell/Cell.gd
+++ characters/cell/Cell.gd
@@ -2,8 +2,22 @@ extends KinematicBody2D
 
 var speed = 300
 var velocity = Vector2.ZERO
+var capacity = 100
+var weight = 0
+
+var inventory = {
+	"water": 0,
+	"carbon": 0,
+	"potassium": 0,
+	"nitrogen": 0,
+	"phosphorus": 0,
+}
+
 
 func _ready():
+	pass
+
+func _process(delta: float):
 	pass
 	
 
@@ -19,7 +33,7 @@ func _physics_process(delta: float):
 	if Input.is_action_pressed("go_left"):
 		velocity.x = -speed
 		
-		
+	
 		
 	move_and_slide(velocity, Vector2.UP)
 	
