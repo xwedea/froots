a0d8667c5412a969fccedf23391b00c4554dfceb
xwedea <mecitjiyan@gmail.com>
2023-02-04 14:47:24 -0500

    level update

---
+++ levels/BaseLevel/MenuButton.gd
@@ -0,0 +1,10 @@
+extends MenuButton
+
+
+func _ready():
+	get_popup().rect_global_position = Vector2(500,500)
+	
+	pass
+
+func _process(delta):
+	pass
---
+++ levels/level1/Level1.gd
@@ -0,0 +1,13 @@
+extends Node
+
+
+
+func _ready():
+	var ActivationAreas = get_tree().get_nodes_in_group("activation")
+	ActivationAreas[0].connect("completed", self, "level_complete")
+	
+	
+	
+func level_complete():
+	get_tree().change_scene("res://levels/level2/Level2.tscn")
+
